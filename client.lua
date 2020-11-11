b58b1fadfba445dc176ebbc4b613ca354588e17f
中汁龍 <72063585+XNORGATE@users.noreply.github.com>
2020-11-12 02:07:03 +0800

    Create client.lua

---
+++ client.lua
@@ -0,0 +1,39 @@
+ESX = nil
+
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(0)
+	end
+end)
+
+RegisterNetEvent('linyun_giveidentity')
+AddEventHandler('linyun_giveidentity', function()
+	local playerPed = PlayerPedId()
+	local coords = GetEntityCoords(playerPed)
+	local closestPlayer, playerDistance = ESX.Game.GetClosestPlayer()
+
+	if closestPlayer ~= -1 and playerDistance <= 3.0 then
+		ESX.ShowNotification('正在給予身分')
+		TriggerServerEvent('ggg', GetPlayerServerId(closestPlayer))
+	else
+		ESX.ShowNotification('附近沒人')
+	end
+	
+end)
+
+RegisterNetEvent('ggg:showAnim')
+AddEventHandler('ggg:showAnim', function(player)
+	loadAnimDict('anim@amb@nightclub@peds@')
+	TaskStartScenarioInPlace(PlayerPedId(), 'WORLD_HUMAN_CLIPBOARD', 0, false)
+	Citizen.Wait(2000)
+	ClearPedTasks(PlayerPedId())
+end)
+
+
+function loadAnimDict(dict)
+	while (not HasAnimDictLoaded(dict)) do
+		RequestAnimDict(dict)
+		Citizen.Wait(0)
+	end
+end
